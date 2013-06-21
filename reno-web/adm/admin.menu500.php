56c8321f12c3d9dac7411ac3857491fa489d992b
BaeHyunJun <hj3bae@gmail.com>
2013-06-21 15:08:07 +0900

    테스트를 해볼까요?

--- reno-web/adm/admin.menu500.php
+++ reno-web/adm/admin.menu500.php
@@ -1,9 +1,10 @@
 <?
 $menu["menu500"] = array (
     array("500000", "홈페이지 분석", "", "admin_home", "icon icon-globe"),
-    array("", "방문 분석", "", "", ""),
-    array("", "유입 분석", "", "", ""),
-    array("", "페이 분석", "", "", ""),
-    array("", "환경 설정", "", "", ""),
+    array("500100", "Naver Analytics", "$g4[admin_path]/homepage_analytics.php", "", ""),
+//     array("", "방문 분석", "", "", ""),
+//     array("", "유입 분석", "", "", ""),
+//     array("", "페이 분석", "", "", ""),
+//     array("", "환경 설정", "", "", ""),
 );
 ?>
